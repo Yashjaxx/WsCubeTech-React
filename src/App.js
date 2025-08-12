099c9e36fb44814725b35a178466ac2369aa9257
Yashjaxx <yashjaxx32746@gmail.com>
2025-08-12 23:46:39 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Header';
+import Footer from './Footer';
 
 function App() {
   let a="Yash";
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       
       <Header/>
+      <Footer/>
       
     </div>
   );
