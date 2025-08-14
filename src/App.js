d210f0725508173fa9d0898fefec90c251164b5b
Yashjaxx <yashjaxx32746@gmail.com>
2025-08-14 16:42:40 +0530

    add new changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import Header from './Header';
 import Footer from './Footer';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 function App() {
   return (
@@ -10,9 +12,22 @@ function App() {
       <Header/>
       <div className='container'>
         <h1>Welcome Yash</h1>
+        <div>
+          <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="holder.js/100px180" />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+        </div>
+
       </div>
-      <Footer/>
-      
+      <Footer/> 
     </div>
   );
 }
