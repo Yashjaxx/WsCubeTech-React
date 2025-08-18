3c6c7ffb9caa4a9495953a14cf58d9b8faf871b8
Yashjaxx <yashjaxx32746@gmail.com>
2025-08-18 15:38:04 +0530

    add new changes

--- src/Footer.js
+++ src/Footer.js
@@ -3,6 +3,6 @@ import React from 'react'
 export default function Footer(props) {
   console.log(props.email)
   return (
-    <h1>Footer Part</h1>
+    <h1>Footer Part {props.email}</h1>
   )
 }
--- src/Header.js
+++ src/Header.js
@@ -2,12 +2,12 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
-function Header(){
+function Header(props){
     return(
         <div>
             <Navbar bg="dark" data-bs-theme="dark">
                 <Container>
-                <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                <Navbar.Brand href="#home">Navbar{props.name}</Navbar.Brand>
                 <Nav className="me-auto">
                     <Nav.Link href="#home">Home</Nav.Link>
                     <Nav.Link href="#features">Features</Nav.Link>
