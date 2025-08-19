3b87bb0b940ed9171e5f3ce6e160520bfc7e1c7e
Yashjaxx <yashjaxx32746@gmail.com>
2025-08-19 23:08:21 +0530

    add new changes

--- src/Footer.js
+++ src/Footer.js
@@ -3,6 +3,9 @@ import React from 'react'
 export default function Footer(props) {
   console.log(props.email)
   return (
-    <h1>Footer Part {props.email}</h1>
+    <div className='demo'>
+      <h1>Footer Part {props.email}</h1>
+      <p>This is a footer part.</p>
+    </div>
   )
 }
