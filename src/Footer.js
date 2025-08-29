8db46d601486ac8427a4861e7f01fae5b37e3e64
Yashjaxx <yashjaxx32746@gmail.com>
2025-08-29 23:53:18 +0530

    add new changes

--- src/Footer.js
+++ src/Footer.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import web from './images/Yash Image.png'
+import Button from 'react-bootstrap/Button';
 
 export default function Footer(props) {
-  console.log(props.email)
+  const showNaam=(name)=>{
+    alert(`Hello, ${name} How Can I Help you?`)
+  }
   return (
     <div className='demo'>
       <img width={60} src={web}></img>
       <h1>Footer Part {props.email}</h1>
+      <Button variant="success" onClick={()=>showNaam('Yash')}>Save </Button>
       <p>This is a footer part.</p>
     </div>
   )
