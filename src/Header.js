532913df17f0022bff1ade2c6da1398baf7f6e9d
Yashjaxx <yashjaxx32746@gmail.com>
2025-08-29 23:29:10 +0530

    add new features

--- src/Header.js
+++ src/Header.js
@@ -3,8 +3,10 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import './Header.css';
 import web from "./images/Yash Image.png";
+import Button from 'react-bootstrap/Button';
 
 function Header(props){
+    // Normal 
     const displayData = () => {
         alert("Are you sure?");
     };
@@ -12,18 +14,25 @@ function Header(props){
         alert("Are you sure?");
     };
 
+    // Argument 
+    const showName=(name)=>{
+        alert(`Hello, ${name} How Can I help you?`)
+    }
+
     return(
         <div>
             <h1 className='demo1'>Header Section</h1>
              <img className='demo1' width={80} src={web}/>
-
-            <button onClick={displayData}>Save</button><br></br>
-             <button onClick={dataOn}>Save</button>
+            <Button variant="success" onClick={displayData}>Save </Button>
+            <br></br>
+            <Button variant="success" onClick={()=>showName('Yash')}>Help </Button>
+            <br></br>
+            <Button variant="danger" onClick={dataOn}>Delete</Button>
            
             <Navbar bg="dark" data-bs-theme="dark">
                 <Container>
                 <Navbar.Brand href="#home">Navbar{props.name}</Navbar.Brand>
-                <Nav className="me-auto">
+                <Nav className="mb-auto">
                     <Nav.Link href="#home">Home</Nav.Link>
                     <Nav.Link href="#features">Features</Nav.Link>
                     <Nav.Link href="#pricing">Pricing</Nav.Link>
